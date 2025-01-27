1c31e4d4046f7a0344dd55a901e8b6145f75abdd
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-27 19:23:08 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import Data from './data';
+import React, { useEffect, useState } from "react";
+
+
+const Blog = ({ initialPosts }) => {
+    const [dynamicPosts, setDynamicPosts] = useState([]);
+
+    useEffect(() => {
+        // Fetch additional data on the client side
+        fetch("https://jsonplaceholder.typicode.com/posts?_start=5&_limit=5")
+            .then((response) => response.json())
+            .then((data) => setDynamicPosts(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Blog Posts</h1>
+            <h2>Server-Side Rendered Posts:</h2>
+            <ul>
+                {initialPosts.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+            <h2>Client-Side Rendered Posts:</h2>
+            <ul>
+                {dynamicPosts.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export async function getServerSideProps() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
+    const initialPosts = await res.json();
+
+    return { props: { initialPosts } };
+}
+
+export default Blog;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
